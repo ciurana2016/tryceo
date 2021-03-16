14e55661074ab0f22df5606e69c8dc55e4c9f82b
Victor Ciurana <admin@victorciurana.com>
2021-03-16 19:21:48 +0100

    clean db and new command to populate it

---
+++ interview/management/commands/populate_db.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+
+from interview.database import filter_and_save_data()
+
+
+
+class Command(BaseCommand):
+
+    """
+        Populates the Django models with data from the remote database.
+    """
+
+    def handle(self, *args, **options):
+        print('[Populate db] START')
+        filter_and_save_data()
+        print('[Populate db] END')
