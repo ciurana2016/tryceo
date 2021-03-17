1a0addeec9aaf2e88c13fc9b84e19dbcc2c6f653
Victor Ciurana <admin@victorciurana.com>
2021-03-17 15:17:09 +0100

    for now slow way

--- interview/management/commands/geocode_hotel_address.py
+++ interview/management/commands/geocode_hotel_address.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 
 from interview.geocode import Geocoder
-
+from interview.models import Hotel
 
 
 class Command(BaseCommand):
@@ -13,6 +13,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print('[Update hotel geodata] START')
 
-        # Call object
+        for idx, hotel in enumerate(Hotel.objects.all()):
+            print(f'\t updating hotel {idx} ...')
+            Geocoder.update_hotel_geodata(hotel)
 
         print('[Update hotel geodata] END')
